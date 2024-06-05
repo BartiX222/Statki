254f063b63f846efee6f34c612be4e3a3dce4872
BartiX222 <barttek.orczykowski@gmail.com>
2024-06-05 10:29:06 +0200

    Add files via upload

---
+++ BattleShip/Board.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace BattleShip
+{
+    public class Board
+    {
+        public char[,] board = new char[10, 10];
+
+        public void SetupBoard()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    board[i, j] = '~';
+                }
+            }
+        }
+
+        public void PrintBoard()
+        {
+            Console.Write("    A   B   C   D   E   F   G   H   I   J");
+            for (int i = 0; i < 10; i++)
+            {
+                if (i + 1 != 10) Console.Write("\n" + (i + 1) + " | ");
+                else Console.Write("\n" + (i + 1) + "| ");
+
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(board[i, j] + " | ");
+                }
+            }
+            Console.WriteLine("");
+        }
+
+        private bool IsPositionValid(int x, int y, int size, bool horizontal)
+        {
+            int startX = Math.Max(0, x - 1);
+            int endX = Math.Min(9, horizontal ? x + 1 : x + size);
+            int startY = Math.Max(0, y - 1);
+            int endY = Math.Min(9, horizontal ? y + size : y + 1);
+
+            for (int i = startX; i <= endX; i++)
+            {
+                for (int j = startY; j <= endY; j++)
+                {
+                    if (board[i, j] == 'S')
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public void PlaceShip(Ship ship)
+        {
+            char[,] backup = board.Clone() as char[,];
+            PrintBoard();
+            Console.WriteLine($"Stawianie statku o dlugosci {ship.Size}");
+            int[] cords = Player.GetPlayerInput();
+
+            if (board[cords[0], cords[1]] == 'S')
+            {
+                Console.WriteLine("Na podanych wspolrzednych znajduje sie juz statek. Sprobuj ponownie.");
+                PlaceShip(ship);
+                return;
+            }
+            if (ship.Size == 1)
+            {
+                if (IsPositionValid(cords[0], cords[1], ship.Size, true))
+                {
+                    board[cords[0], cords[1]] = 'S';
+                }
+                else
+                {
+                    Console.WriteLine("Nie mozna sutawić tutaj statku bo jest za malo miejsca. Sprobuj ponownie.");
+                    PlaceShip(ship);
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Podaj kierunek statku: poziomo( H ) albo pionowo( V ).");
+                string direction = Console.ReadLine().ToLower();
+                if (direction != "h" && direction != "v")
+                {
+                    Console.WriteLine("Podano niepoprawny kierunek. Sprobuj ponownie.");
+                    PlaceShip(ship);
+                    return;
+                }
+                if (direction == "h")
+                {
+                    if (cords[1] + ship.Size > 10)
+                    {
+                        Console.WriteLine("Nie mozna postawic statku poza plansza. Sprobuj ponownie.");
+                        PlaceShip(ship);
+                        return;
+                    }
+                    if (IsPositionValid(cords[0], cords[1], ship.Size, true))
+                    {
+                        for (int i = 0; i < ship.Size; i++)
+                        {
+                            board[cords[0], cords[1] + i] = 'S';
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nie mozna sutawić tutaj statku bo jest za malo miejsca. Sprobuj ponownie.");
+                        PlaceShip(ship);
+                        return;
+                    }
+                }
+                else
+                {
+                    if (cords[0] + ship.Size > 10)
+                    {
+                        Console.WriteLine("Nie mozna postawic statku poza plansza. Sprobuj ponownie.");
+                        PlaceShip(ship);
+                        return;
+                    }
+                    if (IsPositionValid(cords[0], cords[1], ship.Size, false))
+                    {
+                        for (int i = 0; i < ship.Size; i++)
+                        {
+                            board[cords[0] + i, cords[1]] = 'S';
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nie mozna sutawić tutaj statku bo jest za malo miejsca. Sprobuj ponownie.");
+                        PlaceShip(ship);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BattleShip/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+namespace BattleShip
+{
+    internal class Program
+    {
+        void MakeMove(Player player, int x, int y)
+        {
+            Console.WriteLine($"{player} makes a move at ({x}, {y})");
+        }
+        public static void Main(string[] args)
+        {
+            Player player1 = new Player();
+            Player player2 = new Player();
+
+            player1.SetupShips();
+            player1.ownBoard.PrintBoard();
+            Console.Clear();
+            Console.WriteLine("Press any key to change player to player2...");
+            Console.ReadLine();
+            Console.Clear();
+            player2.SetupShips();
+            player2.ownBoard.PrintBoard();
+            Console.Clear();
+            Console.WriteLine("Press any key to change player to player1...");
+            Console.ReadLine();
+            Console.Clear();
+            while (player1.hits < 20 && player2.hits < 20)
+            {
+                Console.Clear();
+                player1.ownBoard.PrintBoard();
+                player1.targetBoard.PrintBoard();
+                bool hit = player1.Shoot(player2);
+                while (hit)
+                {
+                    Console.WriteLine("Trafiony! Strzelaj ponownie.");
+                    player1.ownBoard.PrintBoard();
+                    player1.targetBoard.PrintBoard();
+                    hit = player1.Shoot(player2);
+                }
+                if (player1.hits == 20) break;
+
+                Console.WriteLine("Press any key to change player to player2...");
+                Console.ReadLine();
+                Console.Clear();
+
+                player2.ownBoard.PrintBoard();
+                player2.targetBoard.PrintBoard();
+                hit = player2.Shoot(player1);
+                while (hit)
+                {
+                    Console.WriteLine("Trafiony! Strzelaj ponownie.");
+                    player2.ownBoard.PrintBoard();
+                    player2.targetBoard.PrintBoard();
+                    hit = player2.Shoot(player1);
+                }
+                if (player2.hits == 20) break;
+
+                Console.WriteLine("Press any key to change player to player1....");
+                Console.ReadLine();
+                Console.Clear();
+            }
+
+            if (player1.hits == 20)
+            {
+                Console.WriteLine("Player 1 wins!");
+            }
+            else
+            {
+                Console.WriteLine("Player 2 wins!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
